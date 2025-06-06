9481a365aef3e13d69452db5f6bc336f23d9eceb
guanjie sun <gjsun255@stu.just.edu.cn>
2025-06-06 17:38:47 +0800

    add app_name

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'todo'
+
 urlpatterns = [
     path('', views.todo_list, name='todo_list'),
     path('add/', views.add_todo, name='add_todo'),
