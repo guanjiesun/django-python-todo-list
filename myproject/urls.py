f9c3cb84029d5d79c4533bb6e2d5b069c474c77c
guanjie sun <gjsun255@stu.just.edu.cn>
2025-06-14 13:19:59 +0800

    update

--- myproject/urls.py
+++ myproject/urls.py
@@ -10,11 +10,11 @@ urlpatterns = [
 
     # TODO include 是 Django 提供的一个函数，用来引入其他模块的 URL 配置
     # goods.urls 表示导入 goods 这个 package 的 urls 模块(urls.py)
-    # freight/ 表示给所有 goods 相关的 URL 加一个统一的前缀 freight/
+    # 'goods/' 表示给所有 goods 相关的 URL 加一个统一的前缀 freight/
     # namespace 是 URL 名称的命名空间，它是 Django 为了解决多 app 中 URL 名字冲突、方便反向解析设置的机制
     # namespace 机制需要 app_name 支持，每一个应用的 urls.py 中要定义 app_name 变量, app_name 的值是任意的
-    path('freight/', include('goods.urls', namespace='goods_ns')),
-    path('house/', include('home.urls', namespace='home_ns')),
-    path('cracker/', include("cookies.urls", namespace='cookies_ns')),
+    path('goods/', include('goods.urls', namespace='goods_ns')),
+    path('home/', include('home.urls', namespace='home_ns')),
+    path('cookies/', include("cookies.urls", namespace='cookies_ns')),
     path('todo/', include('todo.urls', namespace='todo'))
 ]
