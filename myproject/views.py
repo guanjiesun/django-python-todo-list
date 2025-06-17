62f3dead4925943ebc80b7bc98a9918eafcd7891
guanjie sun <gjsun255@stu.just.edu.cn>
2025-06-17 18:47:07 +0800

    update

--- myproject/views.py
+++ myproject/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     # 整个项目的主页
-    content = """
-    <h1 style="text-align: center;">WELCOME TO MY HOMEPAGE</h1>"""
+    content = """<h1 style="text-align: center;">WELCOME TO MY HOMEPAGE</h1>"""
 
     return HttpResponse(content)
