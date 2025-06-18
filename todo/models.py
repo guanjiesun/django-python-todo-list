9dc8b5dddb19ec89174b64522cb5ae0714de5374
guanjie sun <gjsun255@stu.just.edu.cn>
2025-06-18 19:32:21 +0800

    add comments

--- todo/models.py
+++ todo/models.py
@@ -4,8 +4,14 @@ from django.db import models
 class TodoItem(models.Model):
     item = models.CharField(max_length=255)
     completed = models.BooleanField(default=False)
+
+    # auto_now_add 设置为 True, 则在调用 .create() 创建对象时会自动设置 created_at 字段的值为当前时间
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
+    # auto_now 设置为 True, 则每次调用 .save() 的时候都会更新 updated_at 字段的值为当前时间
+    # .create() 内部会调用 .save(), 所以在创建对象时 updated_at 字段也会被设置为当前时间
     updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
 
     def __str__(self):
+        # 返回待办事项的内容作为字符串表示
         return self.item
