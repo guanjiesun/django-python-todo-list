487a055c7e2c788e4e6e41803337d1303a589bb2
guanjie sun <gjsun255@stu.just.edu.cn>
2025-07-06 22:05:36 +0800

    add user-agent info funtionality

--- myproject/middleware/log_ip.py
+++ myproject/middleware/log_ip.py
@@ -2,5 +2,6 @@ from django.utils.deprecation import MiddlewareMixin
 
 class LogClientIPMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        ip = request.META.get('REMOTE_ADDR')
-        print(f"[客户端 IP] {ip} 请求了 {request.path}")
+        ip = request.META.get('REMOTE_ADDR', 'GUEST_IP')
+        user_agent = request.META.get('HTTP_USER_AGENT', 'GUEST_AGENT')
+        print(f"[客户端 {user_agent}] {ip} 请求了 {request.path}")
