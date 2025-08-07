627af2782d5fbaaca23116331bd79b9190317def
guanjie sun <gjsun255@stu.just.edu.cn>
2025-08-08 01:12:39 +0800

    use forms system to add a new todo item

--- todo/urls.py
+++ todo/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('<int:item_id>/', views.todo_item, name='todo_item'),
     path('set-session/', views.set_session, name='set_session'),
     path('get-session/', views.get_session, name='get_session'),
+    path('add-todo-item/', views.add_todo_item, name='add_todo_item'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -2,23 +2,25 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseNotAllowed, JsonResponse, HttpResponse
 from django.db import transaction
 from .models import TodoItem
+from .forms import TodoItemForm
 
 import json
 
 
 def index(request):
     """
-    显示所有 items
-    - GET 请求：返回 item 列表页面
-    - POST 请求：创建新 item 然后重定向，发起一次新的 GET 请求 (POST -> redirect -> GET)
-    - 其他请求：返回 405 不允许
+    - GET  请求: 显示所有 todo items
+    - POST 请求: 添加一个新的 todo item. 然后重定向, 发起一次新的 GET 请求 (POST/REDIRECT/GET 模式, 避免重复的 POST 请求)
+    - 其他请求：返回 HTTP 405 Method Not Allowed 错误
     """
     if request.method == 'POST':
         item_text = request.POST.get('item')
         if item_text:
             # 防止 空字符串 或者 None 插入到数据库
             TodoItem.objects.create(item=item_text)
-        # post->redirect->get
+        # redirect 函数会生成一个 HTTP 302 重定向响应, 是实现 POST/Redirect/GET 模式的核心
+        # 浏览器收到这个响应后，会自动发起一个新的 GET 请求到相应的 URL
+        # 需要了解 HTTP 重定向的工作原理, 可以利用 wireshark 观察详细信息
         return redirect('todo:index')
     elif request.method == 'GET':
         items = TodoItem.objects.all().order_by('id')
@@ -32,7 +34,10 @@ def index(request):
 
 def todo_item(request, item_id):
     """
-    查询、修改和删除一个 item
+    - PUT 请求: 更新一个 todo item 的内容
+    - DELETE 请求: 删除一个 todo item
+    - GET 请求: 显示一个 todo item 的详情
+    - 其他请求：返回 HTTP 405 Method Not Allowed 错误
     """
 
     # 若数据库中没有主键为 item_id 的 TodoItem 对象, 立即返回一个 HTTP 404 错误响应
@@ -64,16 +69,16 @@ def todo_item(request, item_id):
 def set_session(request):
     """
     设置用户的 session 数据
-
-    1. Django 的 SessionMiddleware 中间件 会根据 HTTP 请求头中的 Cookie 字段 来判断用户是否已经拥有 sessionid
-    如果有, SessionMiddleware 会从数据库中django_session 表中查询该 session ID 对应的 session 数据
-
-    2. 如果客户端没有携带名为 sessionid 的 Cookie, 只要视图中对 request.session 进行了写操作(如设置某个键值).
-    Django 就会自动创建一个新的 session ID, 并在响应中通过 Set-Cookie 字段将 sessionid 发送给客户端, 
-    同时将会话数据保存到服务器端(如数据库、缓存等 session 后端).
-
-    3. 客户端在后续请求中会自动携带该 session ID, 从而实现会话保持.
     """
+
+    # 1. Django 的 SessionMiddleware 中间件 会根据 HTTP 请求头中的 Cookie 字段 来判断用户是否已经拥有 sessionid
+    # 如果有, SessionMiddleware 会从数据库中django_session 表中查询该 session ID 对应的 session 数据
+
+    # 2. 如果客户端没有携带名为 sessionid 的 Cookie, 只要视图中对 request.session 进行了写操作(如设置某个键值).
+    # Django 就会自动创建一个新的 session ID, 并在响应中通过 Set-Cookie 字段将 sessionid 发送给客户端,
+    # 同时将会话数据保存到服务器端(如数据库、缓存等 session 后端).
+
+    # 3. 客户端在后续请求中会自动携带该 session ID, 从而实现会话保持.
 
     request.session['username'] = 'Guanjie Sun'
     request.session['email'] = 'guanjie.sun@outlook.com'
@@ -89,3 +94,27 @@ def get_session(request):
     username = request.session.get('username', 'Guest')
     email = request.session.get('email', 'No email set')
     return HttpResponse(f'Hello, {username}@{email}!')
+
+
+def add_todo_item(request):
+    """ 利用 Django 的表单系统来添加 todo item """
+    if request.method == 'POST':
+        # 浏览器的刷新（F5）行为 或 点击地址栏再按回车 是“重新发送上一次的请求”
+            # 也就是说：
+            # 如果这个页面是通过 GET 请求获得的 → 刷新会重新发 GET；
+            # 如果这个页面是通过 POST 请求获得的（例如你刚提交了表单）→ 刷新会尝试重新发 POST！
+        # 所以如果用户在添加待办事项后刷新页面，浏览器会重新发送 POST 请求
+        # 这会导致重复添加同一个待办事项
+        # 为了避免这种情况，可以在处理 POST 请求后重定向到 GET 请求
+        # 这样浏览器就会发起新的 GET 请求，而不是重新发送 POST 请求
+        # 这就是所谓的 POST/Redirect/GET 模式
+        form = TodoItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # redirect 函数会生成一个 HTTP 302 重定向响应
+            # 浏览器收到这个响应后，会自动发起一个新的 GET 请求到相应的 URL
+            # 302 的作用就是“Post → Redirect → Get” 模式的核心
+            return redirect('todo:add_todo_item')
+    else:
+        form = TodoItemForm()
+    return render(request, 'todo/add_todo_item.html', {'form': form})
