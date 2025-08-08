201bb5b7d100db9075c4ea8452f50df3cb89ccdb
guanjie sun <gjsun255@stu.just.edu.cn>
2025-08-08 23:08:27 +0800

    update comments

--- todo/apps.py
+++ todo/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class TodoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    default_auto_field = 'django.db.models.BigAutoField'  # 指定这个 todo app 中的模型的主键字段默认使用的类型
     name = 'todo'
--- todo/forms.py
+++ todo/forms.py
@@ -5,4 +5,4 @@ from .models import TodoItem
 class TodoItemForm(forms.ModelForm):
     class Meta:
         model = TodoItem   # 表单是基于 TodoItem 模型的
-        fields = ['item']  # 声明只用模型里的 item 字段作为表单字段 
+        fields = ['item']  # 声明只用 TodoItem 模型里的 item 字段作为表单字段 
